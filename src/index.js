a29a48686d4b5fe6559d8015963e2ac55c6efd39
Jaakko252525 <124563002+Jaakko252525@users.noreply.github.com>
2023-06-26 14:37:52 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ import reducer from './reducers/anecdoteReducer'
 
 const store = createStore(reducer)
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>
 )
+
+
