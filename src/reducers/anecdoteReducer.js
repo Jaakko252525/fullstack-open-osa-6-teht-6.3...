02f0c2986a1819375d279552c851d1147f837637
Jaakko252525 <124563002+Jaakko252525@users.noreply.github.com>
2023-07-03 14:02:55 +0300

    initial commit

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -2,15 +2,6 @@ import { get } from "mongoose"
 import { createSlice } from '@reduxjs/toolkit'
 
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 
 export const voteAnecdote = (id) => {
   console.log('inside voteAnecdote!', )
@@ -53,14 +44,13 @@ export const asObject = (anecdote) => {
 
 
 
-const initialState = anecdotesAtStart.map(puttingDefaultAnecdotes)
 
 
 // new reducer
 const anecdoteSlice = createSlice(
   {
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers: {
     createAnecdote(state, action) {
       const content = action.payload
@@ -69,6 +59,7 @@ const anecdoteSlice = createSlice(
         id: getId(),
         votes: 0
       })
+    
     }, 
     vote(state, action) {
       const id = action.payload.id
@@ -86,12 +77,18 @@ const anecdoteSlice = createSlice(
       console.log('state:',JSON.parse(JSON.stringify(state)))
       return state.map(anecdote => 
         anecdote.id !== id ? anecdote : changedAnecdote)
+    },
+    appendAnecdote(state, action) {
+      state.push(action.payload)
+    },
+    setAnecdotes(state, action) {
+      return action.payload
     }
   }
 })
 
 
-export const { createAnecdote, vote } = anecdoteSlice.actions
+export const { createAnecdote, vote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
 
 
